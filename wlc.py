e1e870f9f0796737e5db49ad561cf5e17c067f7b
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-03-17 14:02:46 +0600

    Update wlc.py

--- wlc.py
+++ wlc.py
@@ -2,7 +2,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Welcome Mao \n"
+m = random.choice(color)+"Welcome THBD3 \n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
